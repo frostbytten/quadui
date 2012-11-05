0c49690e8c9fbe7ce1856ced7ae9bc72e42dbfe0
Christopher Villalobos <christopher.villalobos@gmail.com>
2012-11-05 10:13:08 -0500

    Start ACMO Integration
    * Clean class for Acmo Task
    * Added git.properties to ignore (auto generated by Maven)
    
    Signed-off-by: Christopher Villalobos <christopher.villalobos@gmail.com>

---
+++ src/main/java/org/agmip/ui/quadui/GenerateAcmoTask.java
@@ -0,0 +1,18 @@
+package org.agmip.ui.quadui;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.agmip.util.MapUtil;
+
+import org.apache.pivot.util.concurrent.Task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class GenerateAcmoTask extends Task<HashMap> {
+    @Override
+    public HashMap<String, Object> execute() {
+        return null;
+    }
+}
