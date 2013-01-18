14bb1598dd59fc4759bfd749f2d3c2e3265169d2
MengZhang <meng.zhang15@ufl.edu>
2013-01-18 10:11:41 -0500

    Add CLI logger output

--- src/main/java/org/agmip/ui/quadui/QuadUIApp.java
+++ src/main/java/org/agmip/ui/quadui/QuadUIApp.java
@@ -5,12 +5,16 @@ import org.apache.pivot.collections.Map;
 import org.apache.pivot.wtk.Application;
 import org.apache.pivot.wtk.DesktopApplicationContext;
 import org.apache.pivot.wtk.Display;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class QuadUIApp extends Application.Adapter {
     private QuadUIWindow window = null;
+    private static Logger LOG = LoggerFactory.getLogger(QuadCmdLine.class);
 
     @Override
     public void startup(Display display, Map<String, String> props) throws Exception {
+        LOG.info("Run GUI start");
         BXMLSerializer bxml = new BXMLSerializer();
         window = (QuadUIWindow) bxml.readObject(getClass().getResource("/quadui.bxml"));
         window.open(display);
@@ -25,6 +29,21 @@ public class QuadUIApp extends Application.Adapter {
     }
 
     public static void main(String[] args) {
-        DesktopApplicationContext.main(QuadUIApp.class, args);
+        boolean cmdFlg = false;
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equalsIgnoreCase("-cli")) {
+                cmdFlg = true;
+                break;
+            }
+        }
+        if (cmdFlg) {
+            LOG.info("Run CLI");
+            QuadCmdLine cmd = new QuadCmdLine();
+            cmd.run(args);
+            
+        } else {
+            LOG.info("Run GUI");
+            DesktopApplicationContext.main(QuadUIApp.class, args);
+        }
     }
 }
